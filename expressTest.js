35f4f4c821925ee2770b55653e7d9dc88c06baaf
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-01-28 17:13:39 +0900

    express 백 프론트 연결

--- expressTest.js
+++ expressTest.js
@@ -1,22 +1,29 @@
 const express = require("express");
 const app = express();
+var cors = require("cors");
 var portNo = 3000;
-
+app.use(cors());
 app.get("/", function (req, res) {
   res.send("helloworld");
 });
 
 app.get("/user/:id", function (req, res) {
-  const q = req.params;
-  console.log(q.id);
-  res.json({ userId: q.id });
+  //   const q = req.params;
+  //   console.log(q.id);
+  //   res.json({ userId: q.id });
+  const q = req.query;
+  console.log(q);
 });
 
-app.get("/dog", function (req, res) {
-  res.send({ sound: "멍멍" });
-});
-app.get("/cat", function (req, res) {
-  res.send({ sound: "냐옹" });
+app.get("/sound/:name", (req, res) => {
+  const { name } = req.params;
+  if (name == "dog") {
+    res.json({ sound: "멍멍" });
+  } else if (name == "cat") {
+    res.json({ sound: "야옹" });
+  } else {
+    res.json({ sound: "알수없음" });
+  }
 });
 
 app.listen(portNo, () => {
